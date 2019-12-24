bf7ee7871cea371c888e9a84c8975e990c4c9265
Naldson Chagas <naldson.bc@gmail.com>
2019-12-24 16:35:44 -0300

    has kanal d

--- bot.js
+++ bot.js
@@ -3,32 +3,37 @@ const config = require('./config');
 const twit = require('twit');
 const T = new twit(config);
 
-function retweet() {
-    let params = {
-        q: 'kanal',
-        lang: 'pt',
-        result_type: 'recent',
-        count: 1
-    };
-    T.get('search/tweets', params, (err, data, response) => {
+setInterval(() => {
+    searchTweets();
+}, 60000);
+
+let params = {
+    q: 'kanal',
+    lang: 'pt',
+    result_type: 'recent',
+    count: 1
+};
+
+function searchTweets() {
+    T.get('search/tweets', params, (err, data) => {
         let tweets = data.statuses;
         if (!err) {
             for (const dat of tweets) {
+                console.log(dat);
                 const retweetId = dat.id_str;
-
-                T.post('statuses/retweet/:id', { id: retweetId }, (err, response) => {
-                    if (err) {
-                        console.log(err.message);
-                    } else {
-                        console.log(`Retuitou, macho vei`);
-                    }
-                });
+                retweet(retweetId);
             }
         }
     });
 }
 
-setInterval(() => {
-    retweet();
-}, 15000);
+function hasKanalD(dat) {
+    return !(dat.retweeted_status.text.toLowerCase().indexOf('kanal d') == -1);
+}
 
+function retweet(retweetId) {
+    T.post('statuses/retweet/:id', { id: retweetId }, (err) => {
+        if (err) console.log(err.message);
+        else console.log(`Retuitou, macho vei`);
+    });
+}
