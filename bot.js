537540a5a62575bcdd771ff02e703d3369abcf96
Naldson Chagas <naldson.bc@gmail.com>
2020-05-01 16:22:10 -0300

    dot env

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const config = require('./config');
 
 const twit = require('twit');
@@ -8,7 +10,7 @@ setInterval(() => {
 }, 60000);
 
 let params = {
-    q: 'kanal',
+    q: '',
     lang: 'pt',
     result_type: 'recent',
     count: 1
@@ -22,23 +24,15 @@ function searchTweets() {
                 const retweetId = dat.id_str;
                 const tweetText = dat.text;
 
-                if (hasKanalD(tweetText)) {
-                    console.log(`K-pop fan detected: ${tweetText}`);
-                    break;
-                }
                 retweet(retweetId, tweetText);
             }
         }
     });
 }
 
-function hasKanalD(tweetText) {
-    return !(tweetText.toLowerCase().indexOf('kanal d') == -1);
-}
-
 function retweet(retweetId, tweetText) {
     T.post('statuses/retweet/:id', { id: retweetId }, (err) => {
         if (err) console.log(err.message);
-        else console.log(`Retuitou, macho vei: ${tweetText}`);
+        else console.log(`RT success: ${tweetText}`);
     });
 }
